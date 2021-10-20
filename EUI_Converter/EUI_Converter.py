0b6e3d2e61f74dd38b1a6a233252f4aedc789c70
Rsge <rsge@web.de>
2021-10-20 21:32:22 +0200

    Added the ability to only add CUC to EUI.

--- EUI_Converter/EUI_Converter.py
+++ EUI_Converter/EUI_Converter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#Imports
+# Imports
 import os
 from os.path import join as j
 from glob import glob as g
@@ -8,56 +8,67 @@ import subprocess
 import shutil
 import re
 
-#Change to base DLC directory
-os.chdir("../..")
 
-##Global Values
+## Global Values
 print("Configuring variables...")
-#Names
-# Customize according to your setup here #
+
+# Names
+# Customize these according to your setup #
 vanilla_eui_zip = "!EUI.7z" # Vanilla EUI file (in vanilla_packs_folder)
 modded_eui_zip = "!EUI_CUC.7z" # CUC-version file name of EUI
 modsave_folder = "zzz_Modsaves" # Folder containing this scripts project folder and it's needed edited files
 vanilla_packs_folder = "zz_Vanilla_Versions" # Folder containing the vanilla packs
-# Don't change from here [if you don't really know what you're doing that is of cause ;)] #
+# Ignore these #
 modpack_folder_name = "MP_MODSPACK"
-eui_cu_file_names = ["CityBannerManager.lua",
+eui_cuc_file_names = ["CityBannerManager.lua",
                      "CityView.lua",
                      "Highlights.xml"]
 load_tag = "ContextPtr:LoadNewContext"
 unit_panel_file_name = "UnitPanel.lua"
 ige_compat_file_name = "IGE_Window.lua"
-delete_file_names = ["CivilopediaScreen.lua",
+# Maybe you also need to add to these lists #
+delete_file_names = ["CivilopediaScreen.lua", # Mod files overwriting original UI files, conflicting with EUI
                      "CityView.lua",
                      "TechTree.lua",
                      "TechButtonInclude.lua",
                      unit_panel_file_name]
-unit_panel_modcompat_file_names = ["EvilSpiritsMission.lua",
+unit_panel_modcompat_file_names = ["EvilSpiritsMission.lua", # Mod files adding new actions to units
                                    "THTanukiMission.lua"]
-#Paths
+
+# Don't change from here [if you don't really know what you're doing that is of cause ;)] #
+
+# This only runs with 7zip. If you want to use WinRar you'll have to change the methods yourself - or just download 7zip =P
+szip = r"C:\Program Files\7-Zip\7z.exe"
+
+# Paths
+# Change to base DLC directory
+os.chdir("../..")
 base_path = os.getcwd()
 modsave_path = j(base_path, modsave_folder)
 modpack_path = j(base_path, modpack_folder_name)
 vanilla_packs_path =  j(base_path, vanilla_packs_folder)
 ui_path = j(modpack_path, "UI")
 eui_path = j(base_path, "UI_bc1")
-szip = r"C:\Program Files\7-Zip\7z.exe"
-#Files
+# Files
 base_eui_zip_path = j(vanilla_packs_path, vanilla_eui_zip)
 modded_eui_zip_path = j(base_path, modded_eui_zip)
 mod_files = j(modpack_path, "Mods", "**", "*.lua")
 ui_files = j(ui_path, "*.lua")
 eui_files = j(eui_path, "*", "*.lua")
 
-#Global Variables
+# Global Variables
 load_tags = {}
 unit_panel_modcompat_needed = False
 null = open(os.devnull, 'w')
+eui_only = False
 
 
-#Get modpack zip
+# Get modpack zip
 while True:
-    modpack_name = input("\nWhich pack should be converted?\n")
+    modpack_name = input("\nWhich pack should be converted?\n(Type 'EUI' to just convert EUI)\n")
+    if modpack_name == "EUI":
+        eui_only = True
+        break
     modpack_zips = g(j(vanilla_packs_path, modpack_name + ".*"))
     if len(modpack_zips) > 0:
         modpack_zip = modpack_zips[0]
@@ -65,57 +76,65 @@ while True:
     print("This file doesn't exist, try again.")
 
 
-#Remove previous modpack
+# Remove previous modpack
 print("Removing previous modpack leftovers...")
 if os.path.isdir(modpack_path):
     shutil.rmtree(modpack_path)
 if os.path.isdir(eui_path):
     shutil.rmtree(eui_path)
 
-#Compile EUI with colored unlocked citizens
+# Compile EUI with colored unlocked citizens
 if not os.path.isfile(modded_eui_zip_path):
     print("Creating colored unlocked Citizens EUI...")
     subprocess.run([szip, 'x', base_eui_zip_path], stdout=null, stderr=null)
     #shutil.move(j(vanilla_packs_path, eui_file_name), eui_path)
-    for eui_cu_file_name in eui_cu_file_names:
-        eui_cu_file = g(j(modsave_path, eui_cu_file_name + "*"))[0]
-        orig_eui_file = g(j(eui_path, "*", eui_cu_file_name))[0]
+    for eui_cuc_file_name in eui_cuc_file_names:
+        eui_cuc_file = g(j(modsave_path, eui_cuc_file_name + "*"))[0]
+        orig_eui_file = g(j(eui_path, "*", eui_cuc_file_name))[0]
         shutil.move(orig_eui_file, orig_eui_file + ".orig")
-        shutil.copyfile(eui_cu_file, orig_eui_file)
+        shutil.copyfile(eui_cuc_file, orig_eui_file)
     subprocess.run([szip, 'a', modded_eui_zip, eui_path], stdout=null, stderr=null)
-else:
-    #Unzip EUI
+elif not eui_only:
+    # Unzip EUI
     print("Unzipping EUI...")
     subprocess.run([szip, 'x', modded_eui_zip_path], stdout=null, stderr=null)
 
+# Stop here if only EUI should be converted
+if eui_only:
+    if os.path.isdir(eui_path):
+        shutil.rmtree(eui_path)
+    null.close()
+    print("Done.\n")
+    exit(0)
 
-#Unzip modpack zip
+
+# Unzip modpack zip
 print("Unzipping Modpack...")
 subprocess.run([szip, 'x', j(vanilla_packs_path, modpack_zip)], stdout=null, stderr=null)
 
 
-#Manage mod files
+# Manage mod files
 for mod_file in g(mod_files, recursive = True):
     mod_file_path = mod_file.split(os.sep)
     mod_file_name = mod_file_path[len(mod_file_path) - 1]
 
-    #IGE UI compat file
+    # IGE UI compat file
     if mod_file_name == ige_compat_file_name:
         print("Providing IGE-EUI-compat...")
         shutil.move(mod_file, mod_file + ".orig")
         shutil.copyfile(g(j(modsave_path, ige_compat_file_name + "*"))[0], mod_file)
 
-    #Delete UI overwrite duplicates
+    # Delete UI overwrite duplicates
     if mod_file_name in delete_file_names:
         print("Removing overwriting file " + mod_file_name + "...")
         os.remove(mod_file)
 
-    #Find out if modcompat unit panel needed
+    # Find out if modcompat unit panel needed
     if mod_file_name in unit_panel_modcompat_file_names:
         print("UnitPanel modcompat need detected...")
         unit_panel_modcompat_needed = True
 
-#Delete useless desktop.ini (Thanks True...)
+# Delete useless desktop.ini (Thanks True...)
 ini_files = re.sub(r"\.\w+$", ".ini", mod_files)
 for ini_file in g(ini_files, recursive = True):
     ini_file_path = ini_file.split(os.sep)
@@ -125,7 +144,7 @@ for ini_file in g(ini_files, recursive = True):
         os.remove(mod_file)
 
 
-#Get stuff from UI files
+# Get stuff from UI files
 for ui_file in g(ui_files):
     with open(ui_file, 'r') as file:
         lines = file.readlines()
@@ -141,38 +160,39 @@ for ui_file in g(ui_files):
         if line.startswith(load_tag):
             load_tags[ui_file_name].append(line)
 
-#Insert stuff into EUI files
+# Insert stuff into EUI files
 for eui_file in g(eui_files):
     eui_file_path = eui_file.split(os.sep)
     eui_file_name = eui_file_path[len(eui_file_path) - 1]
 
-    #Base UI files
+    # Base UI files
     if eui_file_name in load_tags.keys():
         print("Writing tags to " + eui_file_name + "...")
         with open(eui_file, 'a') as file:
             file.write('\n')
             for load_tag in load_tags[eui_file_name]:
                 file.write(load_tag)
-    #Modcompat unit panel
+    # Modcompat unit panel
     elif eui_file_name == unit_panel_file_name and unit_panel_modcompat_needed:
         print("Providing EUI-UnitPanel-Modcompat...")
         shutil.move(eui_file, eui_file + ".orig")
         shutil.copyfile(g(j(modsave_path, unit_panel_file_name + "*"))[0], eui_file)
 
 
-#Move EUI folder
+# Move EUI folder
 print("Moving EUI folder...")
 shutil.move(eui_path, modpack_path)
 print("Removing UI folder...")
 shutil.rmtree(ui_path)
 
-#Zip modpack folder
+# Zip modpack folder
 print("Zipping Modpack...")
 subprocess.run([szip, 'a', modpack_name + "_EUI.7z", modpack_path], stdout=null, stderr=null)
 
-##Move modpack folder
+# Move modpack folder
 #print("Moving Modspack folder")
 #shutil.move(modpack_path, j(base_path, modpack_folder_name))
 
 null.close()
 print("Done.\n")
+exit(0)
